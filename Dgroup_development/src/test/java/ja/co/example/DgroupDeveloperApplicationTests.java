79a84502c2deef8a7b2b592786b5e682afb34928
miyagiryusei <20miyagi_ryuusei@axiz.co.jp>
2020-06-12 18:16:57 +0900

    プロジェクトを作成

---
+++ Dgroup_development/src/test/java/ja/co/example/DgroupDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package ja.co.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgroupDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
