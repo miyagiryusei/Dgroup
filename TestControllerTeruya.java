75dcbdd4e9c2d96c03d9532be76f2315c74deedc
kazuma-oshiro <66760564+kazuma-oshiro@users.noreply.github.com>
2020-06-15 12:36:15 +0900

    Add files via upload
    確認画面の追加

---
+++ TestControllerTeruya.java
@@ -0,0 +1,68 @@
+package ja.co.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ja.co.example.form.LoginForm;
+
+@Controller
+public class TestControllerTeruya {
+
+	//ランキング画面表示
+	@RequestMapping("/ranking")
+	public String ranking(Model model) {
+		return "ranking";
+	}
+
+	//全体ランキング遷移
+	@RequestMapping("/overallRanking")
+    public String overallRanking(Model model) {
+        return "overallRanking";
+    }
+
+	//全体ランキング遷移
+		@RequestMapping("/personalRanking")
+	    public String personalRanking(Model model) {
+	        return "personalRanking";
+	    }
+
+	//ログイン画面へ
+		@RequestMapping("/login")
+	    public String login(@ModelAttribute("loginForm") LoginForm loginForm, Model model) {
+	        return "login";
+	    }
+
+	//完了画面へ
+		@RequestMapping("/result")
+	    public String result(@ModelAttribute("loginForm") LoginForm loginForm, Model model) {
+	        return "result";
+	    }
+
+	//Myページ画面表示
+	    @RequestMapping("/myPage")
+		public String myPage(Model model) {
+		    return "myPage";
+		}
+
+	//ポーカー画面
+	  @RequestMapping("/poker")
+	  public String poker(Model model) {
+		  return "poker";
+	  }
+
+	//ブラックジャック画面
+	  @RequestMapping("/blackjack")
+	  public String blackjack(Model model) {
+		  return "blackjack";
+	  }
+	  //確認画面へ
+	//完了画面へ
+			@RequestMapping("/check")
+		    public String check(@ModelAttribute("checkForm") LoginForm loginForm, Model model) {
+		        return "check";
+		    }
+
+
+}
