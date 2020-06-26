a1bbf266c9736cb32a9164a46da7cc589887e821
miyagiryusei <20miyagi_ryuusei@axiz.co.jp>
2020-06-26 10:44:21 +0900

    解消

--- Dgroup_development/src/main/resources/static/js/poker.js
+++ Dgroup_development/src/main/resources/static/js/poker.js
@@ -144,17 +144,17 @@ function firstchangecard() {
 }
 
 function nextchangecard() {
-//     document.card1.src = image[0]['src'];
-//     document.card2.src = image[1]['src'];
-//     document.card3.src = image[2]['src'];
-//     document.card4.src = image[3]['src'];
-//     document.card5.src = image[4]['src'];
+     document.card1.src = image[0]['src'];
+     document.card2.src = image[1]['src'];
+     document.card3.src = image[2]['src'];
+     document.card4.src = image[3]['src'];
+     document.card5.src = image[4]['src'];
 
- document.card1.src = "css/tramp/s01.png";
- document.card2.src = "css/tramp/d02.png";
- document.card3.src = "css/tramp/h04.png";
- document.card4.src = "css/tramp/s05.png";
- document.card5.src = "css/tramp/d08.png";
+// document.card1.src = "css/tramp/s01.png";
+// document.card2.src = "css/tramp/d02.png";
+// document.card3.src = "css/tramp/h04.png";
+// document.card4.src = "css/tramp/s05.png";
+// document.card5.src = "css/tramp/d08.png";
 // document.card5.src = "css/tramp/joker.png";
 //     image = [];
 //     imagenumber = [];
